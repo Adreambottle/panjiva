454832c5ebb9771796e785d6af206c0d147eae3a
Daniel Zhou <31249310+Adreambottle@users.noreply.github.com>
2021-06-29 13:14:18 +0800

    gaibian

---
+++ panjiva_code/Query01.sql
@@ -0,0 +1,17 @@
+
+SELECT
+    panjivarecordid,
+    arrivaldate,
+    conciqcompanyid,
+    conultcompanyid,
+    shpmtorigin,
+    shpciqcompanyid,
+    shpultcompanyid,
+    quantity_num,
+    unit,
+    weightkg,
+    valueofgoodsusd,
+    hscode
+FROM lcusimport{}
+
+-- TO 'C:\Users\Wu Jing\Documents\GitHub\panjiva_data\test{}.csv' DELIMITER ',' CSV HEADER
